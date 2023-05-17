a3cb1b79c7077a400632d34dceb2cc621fdace0f
Pablo <pablo.silva23@fatec.sp.gov.br>
2023-05-17 17:02:58 -0300

    fim da apostila chat01

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
 import os
+import openai
 
 from dotenv import load_dotenv
 load_dotenv()
-print(os.getenvb('OPENAI_API_KEY'))
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+pergunta_padrao = 'Qual o sentimento da seguinte frase? Responda com apenas uma palavra: Positivo, Negativo ou Neutro. Eis a frase: '
+
+frase_do_usuario = input('Escreva uma linha para enviarmos para o ChatGPT\n')
+
+prompt = f'{pergunta_padrao}{frase_do_usuario}'
+
+resposta = openai.Completion.create(
+
+engine='text-davinci-003',
+
+prompt=prompt
+
+)
+
+print ( resposta .choices[0].text)
